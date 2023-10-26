5c3bf8335ac38862bd06ff15d9578e9fb35740b6
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-26 15:49:25 -0300

    Avante Banking - Banco Digital - NextJS 🚀 🔄 🌐

--- src/components/Symmetrical.tsx
+++ src/components/Symmetrical.tsx
@@ -5,54 +5,58 @@ import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 const Symmetrical = () => {
   return (
-    <Flex w="100%" h="100vh" direction="row">
 
-      <Box flex="1" p={8} textAlign="left" backgroundColor="#0F4F2D">
+    <Flex 
+    w="100%" 
+    h="100vh" 
+    direction={{ base: "column", md: "row" }}
+  >
+    <Box flex="1" p={8} textAlign="left" backgroundColor="#0F4F2D">
+      <Image 
+        src="/asserts/card-woman-cell.jpg"
+        alt="Descrição da imagem"
+        display="block"
+        width="100%"
+        height={{ base: "200px", md: "420px" }}
+        maxW="100%"
+        objectFit="cover"
+      />
+      <Heading as="h1" size="2xl" marginBottom="1rem" color="white">
+        Conta do Avante Bank
+      </Heading>
+      <Heading as="h4" size="md" marginBottom="2rem" color="white">
+        Manter suas contas em dia nunca foi tão simples com AI Assistente.
+      </Heading>
+      <Button rightIcon={<ArrowForwardIcon />} colorScheme="#DDC20C" color="#DDC20C" variant="outline">
+        Saiba mais
+      </Button>
+    </Box>
 
-        <Image 
-          src="asserts/card-woman-cell.jpg"
-          alt="Descrição da imagem"
-          display="block"
-          width="100%"
-          height="420px"
-          maxW="100%"
-          objectFit="cover"
-        />
-        <Heading as="h1" size="2xl" marginBottom="1rem" color="white">
-          Conta do Avante Bank
-        </Heading>
-        <Heading as="h4" size="md" marginBottom="2rem" color="white">
-          Manter suas contas em dia nunca foi tão simples com AI Assistente.
-        </Heading>
-        <Button rightIcon={<ArrowForwardIcon />} colorScheme="#DDC20C" color="#DDC20C" variant="outline">
-          Saiba mais
-        </Button>
-      </Box>
+    {/* Lado Direito */}
+    <Box flex="1" p={8} textAlign="left" backgroundColor="#349431">
+      <Image
+        src="/asserts/man-in-a-suit-sitting.jpg"
+        alt="Descrição da imagem"
+        display="block"
+        width="100%"
+        height={{ base: "200px", md: "420px" }}
+        maxW="100%"
+        objectFit="cover"
+      />
+      <Heading as="h1" size="2xl" marginBottom="1rem" color="white">
+        Conta PJ
+      </Heading>
+      <Heading as="h4" size="md" marginBottom="2rem" color="white">
+        Ficou mais fácil organizar o seu negócio com a conta PJ do Avante Bank.
+      </Heading>
 
-      {/* Lado Direito */}
-      <Box flex="1" p={8} textAlign="left" backgroundColor="#349431">
-        <Image
-          src="asserts/man-in-a-suit-sitting.jpg"
-          alt="Descrição da imagem"
-          display="block"
-          width="100%"
-          height="420px"
-          maxW="100%"
-          objectFit="cover"
-        />
-        <Heading as="h1" size="2xl" marginBottom="1rem" color="white">
-          Conta PJ
-        </Heading>
-        <Heading as="h4" size="md" marginBottom="2rem" color="white">
-          Ficou mais fácil organizar o seu negócio com a conta PJ do Avante Bank.
-        </Heading>
-
-        <Button rightIcon={<ArrowForwardIcon />} colorScheme="#DDC20C" color="#DDC20C" variant="outline">
-          Saiba mais
-        </Button>
-      </Box>
-    </Flex>
+      <Button rightIcon={<ArrowForwardIcon />} colorScheme="#DDC20C" color="#DDC20C" variant="outline">
+        Saiba mais
+      </Button>
+    </Box>
+  </Flex>
   );
 };
 
 export default Symmetrical;
+
