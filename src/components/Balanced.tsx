896448d6a30b00c27bda857fd10b29d8cbf44b3a
Emerson Amorim <emerson_tecno@hotmail.com>
2023-10-26 15:46:38 -0300

    Avante Banking - Banco Digital - NextJS 🚀 🔄 🌐

--- src/components/Balanced.tsx
+++ src/components/Balanced.tsx
@@ -1,53 +1,47 @@
-import { Box, Heading, Button, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, Icon } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import Image from 'next/image';
 
 const Balanced = () => {
-  // Use breakpoints para definir o tamanho da imagem de acordo com a largura da tela
-  const imageSize = useBreakpointValue({ base: '80%', md: '609px' });
-
   return (
     <Box
-      w="full"
-      minH="100vh"
+      w="99vw"
+      h="100vh"
       bg="gray.100"
       display="flex"
-      flexDirection={{ base: "column", md: "row" }}
       alignItems="center"
       justifyContent="space-between"
       padding="2rem"
     >
-      <Box
-        textAlign={{ base: "center", md: "left" }}
-        marginBottom={{ base: "2rem", md: "0" }}
-      >
+      <Box>
         <Heading as="h1" size="xl" marginBottom="1rem" color="yellow.400">
           Conheça nosso Cartão de Crédito
         </Heading>
         <Heading as="h3" size="md" fontWeight="normal" marginBottom="1rem">
           Avante Banking: O banco que ilumina seu futuro.
         </Heading>
-        <Button 
-          rightIcon={<ArrowForwardIcon />} 
-          bg="#DDC20C" 
-          borderColor="#DDC20C" 
-          color="black" 
+        <Button
+          rightIcon={<ArrowForwardIcon />}
+          bg="#DDC20C"
+          borderColor="#DDC20C"
+          color="black"
           variant="outline"
         >
           Saiba mais
         </Button>
       </Box>
 
-      <Box 
+      <Box
         display="block"
         maxWidth="78%"
-        mx="auto" // Centraliza a imagem em telas pequenas
+        marginRight="20px"
+        float="right"
       >
-        <Image 
+        <Image
           src="/asserts/yellow-credit-card-avant-bank.jpg"
           alt="Imagem Descrição"
-          width={imageSize}
-          height={imageSize}
+          width={609}
+          height={600}
         />
       </Box>
     </Box>
@@ -55,4 +49,3 @@ const Balanced = () => {
 };
 
 export default Balanced;
-
